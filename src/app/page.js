10bbb3bc6fcd24cc3bb52e6ac951d18330ec3538
nicolasfvp <nicolasfvp11@gmail.com>
2024-08-23 01:29:25 -0300

    Adicionado modais de recuperação de senha

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,10 @@ export default function Login() {
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [showPassword, setShowPassword] = useState(false); 
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [forgotEmail, setForgotEmail] = useState('');
+  const [forgotEmailError, setForgotEmailError] = useState('');
+  const [showEmailSentModal, setShowEmailSentModal] = useState(false);
 
   const router = useRouter();
 
@@ -33,7 +37,7 @@ export default function Login() {
     return passwordRegex.test(password);
   };
 
-  //verificação de email e senha no login
+  //verificação de e-mail e senha no login
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -64,6 +68,27 @@ export default function Login() {
       }
     }
   };
+
+  //verificação de e-mail no recuperar senha
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(forgotEmail)) {
+      setForgotEmailError('E-mail inválido. Insira um endereço de e-mail no formato correto.');
+    } else {
+      const user = users.find(user => user.email === forgotEmail)
+      if(user){
+        setForgotEmailError('');
+        setShowForgotPassword(false);
+        setShowEmailSentModal(true);
+        // Lógica para enviar e-mail de recuperação
+      } else{
+        setForgotEmailError('E-mail inválido. Este endereço de e-mail não está cadastrado no sistema, verifique e tente novamente.')
+      }
+       
+    }
+  };
+
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -104,7 +129,13 @@ export default function Login() {
             {passwordError && <p className="text-red-500 text-sm mt-1">{passwordError}</p>}
           </div>
           <div className="mb-4 text-right">
-            <a href="#" className="text-blue-500 text-sm">Esqueci minha senha</a>
+            <button
+              type="button"
+              onClick={() => setShowForgotPassword(true)}
+              className="text-blue-500 text-sm"
+            >
+              Esqueci minha senha
+            </button>
           </div>
           <button
             type="submit"
@@ -114,6 +145,61 @@ export default function Login() {
           </button>
         </form>
       </div>
+      {/* Modal de Recuperação de Senha */}
+      {showForgotPassword && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+            <h3 className="text-xl text-gray-700 font-bold mb-4">Recuperar Senha</h3>
+            <form onSubmit={handleForgotPassword}>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">E-mail</label>
+                <input
+                  type="text"
+                  value={forgotEmail}
+                  onChange={(e) => setForgotEmail(e.target.value)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"
+                  placeholder="Digite seu e-mail"
+                  autoComplete="off"
+                />
+                {forgotEmailError && <p className="text-red-500 text-sm mt-1">{forgotEmailError}</p>}
+              </div>
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={() => setShowForgotPassword(false)}
+                  className="bg-gray-500 text-white font-bold py-2 px-4 rounded-md hover:bg-gray-600"
+                >
+                  Cancelar
+                </button>
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600"
+                >
+                  Enviar Link
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {/* Modal de E-mail Enviado */}
+      {showEmailSentModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+            <h3 className="text-xl text-gray-700 font-bold mb-4">E-mail Enviado</h3>
+            <p className="text-x1 text-gray-700">Um link de recuperação de senha foi enviado para o seu e-mail.</p>
+            <div className="flex justify-end mt-4">
+              <button
+                onClick={() => setShowEmailSentModal(false)}
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600"
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
